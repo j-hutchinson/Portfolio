dc0460230cced806497bfad6942706ecba2f781f
Jack Hutchinson <jackhutchy@hotmail.com>
2021-03-14 14:31:29 +0000

    mend

--- src/components/Bio/component.tsx
+++ src/components/Bio/component.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { StyledContainer } from './styles';
 
-const Bio = (): JSX.Element => <div>hello</div>
+const Bio = (): JSX.Element => (
+    <StyledContainer>
+        Hello wanderer, I'll finish this section later. If you made it this far, go say hello on one of my social medias
+    </StyledContainer>
+);
 
 export default Bio;
