749a04b76dcb4b51c16e315f8e95b9fd5dc0f734
Jack Hutchinson <jackhutchy@hotmail.com>
2021-03-21 18:56:22 +0000

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './components/App/component';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 render(
   <StrictMode>
@@ -8,3 +10,6 @@ render(
   </StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
+reportWebVitals();
